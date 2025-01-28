5c590c7bb51e941ab01ad8aacb0676bd472c3a91
Sprt6I <Sprt6I@proton.me>
2025-01-28 17:47:27 +0000

    Working game [Alpha]

--- main.py
+++ main.py
@@ -8,12 +8,41 @@ class App__(QMainWindow):
     shapes = ['triangle', 'square', 'circle', 'star']
     colors = ['red', 'green', 'blue', 'pink']
     
-    moves = [] # [ [position, color, shape, number] ]
+    # moves = [] # [ [position, color, shape, number] ]
     def __init__(self):
         super().__init__()
         
         self.setWindowTitle("Sprt6 I\'s Dual N Back")
         self.setGeometry(200, 200, 500, 500)
+        
+        # For counting points
+        self.position_overall = 0 # how many times postition was same
+        self.position_good = 0    # how many times it was clicked at good time
+        self.position_wrong = 0   # how many times someone missclicked
+        
+        self.number_overall = 0
+        self.number_good = 0
+        self.number_wrong = 0
+        
+        self.color_overall = 0
+        self.color_good = 0
+        self.color_wrong = 0
+        
+        self.shape_overall = 0
+        self.shape_good = 0
+        self.shape_wrong = 0
+        
+        # 
+        self.clicked_a = False # Postion
+        self.clicked_w = False # Number
+        self.clicked_s = False # Color
+        self.clicked_d = False # Shape
+        
+        self.same_position = False
+        self.same_number = False
+        self.same_color = False
+        self.same_shape = False
+    
         
         self.nodes = {}
         indx = 0
@@ -56,8 +85,10 @@ class App__(QMainWindow):
         self.setCentralWidget(self.main_widget)
         
     def Main_Game(self):
-        self.moves = 25
+        self.moves = 25 # number of nodes in round
         self.counter = 0
+        self.back = 1
+        self.history = [None] * self.back # [ [position, number, color, shape] ]
         
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.Update_Game)
@@ -66,8 +97,56 @@ class App__(QMainWindow):
     def Update_Game(self):
         if self.counter >= self.moves:
             self.timer.stop()
+        print(self.clicked_a, self.clicked_w, self.clicked_s, self.clicked_d)
+        print(self.same_position, self.same_number, self.same_color, self.same_shape)
         
-        self.Create_Node()
+        if self.same_position:
+            self.position_overall += 1
+            if self.clicked_a:
+                self.position_good += 1
+        else:
+            if self.clicked_a: self.position_wrong += 1
+            
+        if self.same_number:
+            self.number_overall += 1
+            if self.clicked_w:
+                self.number_good += 1
+        else:
+            if self.clicked_w: self.number_wrong += 1
+            
+        if self.same_color:
+            self.color_overall += 1
+            if self.clicked_s:
+                self.color_good += 1
+        else:
+            if self.clicked_s: self.color_wrong += 1
+            
+        if self.same_shape:
+            self.shape_overall += 1
+            if self.clicked_d:
+                self.shape_good += 1
+        else:
+            if self.shape_a: self.shape_wrong += 1
+            
+        self.same_position, self.same_number, self.same_color, self.same_shape = False, False, False, False
+        self.clicked_a, self.clicked_w, self.clicked_s, self.clicked_d = False, False, False, False
+        
+        x = self.Create_Node()
+        
+        if self.history[0]:
+            if x[0] == self.history[0][0]: self.same_position = True
+            if x[1] == self.history[0][1]: self.same_number = True
+            if x[2] == self.history[0][2]: self.same_color = True
+            if x[3] == self.history[0][3]: self.same_shape = True
+        #print(x, self.history[0])
+        
+        for i in range(self.back):
+            if i + 1 < self.back:
+                self.history[i] = self.history[i+1]
+        self.history[-1] = x
+        print(self.history)
+        print()
+        
         self.counter += 1
         
      
@@ -91,6 +170,8 @@ class App__(QMainWindow):
         self.nodes[position][3].show()
         self.nodes[position][3].setText(f'{number}')
         
+        return [position, number, color, shape]
+        
         
         
     def keyPressEvent(self, event: QKeyEvent):
@@ -98,6 +179,11 @@ class App__(QMainWindow):
         key = event.key()
         print(f"Key Pressed: {event.text()} (Code: {key})")
         if event.text() == 'e': self.Main_Game()
+        elif event.text() == 'a': self.clicked_a = True
+        elif event.text() == 'w': self.clicked_w = True
+        elif event.text() == 's': self.clicked_s = True
+        elif event.text() == 'd': self.clicked_d = True
+        
     
 app = QApplication()
 win = App__()
